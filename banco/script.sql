b5850bc2f98a008d8e77e3990a46753db6ade5dc
murilo-martinez <murilo.martinez@sptech.school>
2024-04-28 18:14:14 -0300

    Criação da tabela contato

--- banco/script.sql
+++ banco/script.sql
@@ -1,2 +1,9 @@
 create database mzfc;
 use mzfc;
+
+create table contato(
+	idContato int primary key auto_increment,
+	nome varchar(45) not null,
+	email varchar(45) not null,
+	mensagem varchar(150) not null
+);
